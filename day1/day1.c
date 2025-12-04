cccfc77d575c07d8630e53076e798172bda4ddd1
Jérémy Bourg <jer.bourg2@gmail.com>
2025-12-04 17:27:28 -0500

    day 1 part 1

---
+++ day1/day1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LEFT -1
+#define RIGHT 1
+
+void solve(FILE *input) {
+	char s[100];
+	int pos = 50;
+	int count = 0;
+	while(fgets(s, sizeof(s), input)) {
+		int dir = RIGHT;
+		char *m = s+1;
+
+		if(s[0] == 'L')
+			dir = LEFT;
+		pos = (pos + (atoi(m) * dir));
+		if(pos <= 0 || pos > 100) {
+			count++;
+			pos %= 100;
+		}
+	}
+	printf("%d\n", count);
+}
+
+int main(int argc, char *argv[]) {
+	FILE *fptr;
+
+	fptr = fopen(argv[1], "r");
+
+	if(argc != 2) {
+		fprintf(stderr, "Usage: ./%s [filename]\n", argv[0]);
+		return 0;
+	}
+	if(fptr == NULL) {
+		fprintf(stderr, "File provided doesnt exist\n");
+		return 0;
+	}
+
+	solve(fptr);
+
+	fclose(fptr);
+	return 0;
+}
